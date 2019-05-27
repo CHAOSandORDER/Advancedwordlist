2d816b94ffbc589e10c6dae066f227fd8481e4d6
Avishek <50791518+CHAOSandORDER@users.noreply.github.com>
2019-05-27 23:05:52 +0530

    Add files via upload

---
+++ f.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# mv c.py x
+# mv cupp.cfg x
+mv .eggs x
+mv tests x
+mv Mentalist.egg-info x
+mv pyinstaller_stub.py x
+mv pytest.ini x
+mv mentalist x
+mv mentalist.spec x
+mv MANIFEST.in x
+mv build x
+mv dist x
+mv m.py x
+
--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+mkdir x
+
+chmod +x c.py
+chmod +x r.sh
+chmod +x f.sh
+./f.sh
+rm f.sh
+cd x
+chmod +x m.py
+cd ..
+clear
 
 echo "
   ___   ___ _   _____   _  ______________    _      ______  ___  ___  __   ______________
@@ -8,33 +19,44 @@ echo "
 /_/ |_/____/|___/_/ |_/_/|_/\___/___/____/   |__/|__/\____/_/|_/____/____/___/___/ /_/    
                                                                                           "
 
-mkdir x
-
-mv cupp.cfg x
-mv tests x
-mv Mentalist.egg-info x
-mv pyinstaller_stub.py x
-mv pytest.ini x
-mv mentalist x
-mv mentalist.spec x
-mv MANIFEST.in x
-mv build x
-mv dist x
-mv m.py x
-mv c.py x
-
-cd x
-
-chmod +x c.py
-chmod +x m.py
-
 # run_cupp.py
 
 python3 c.py -i
 
 # run_mentalist
 
-python3 m.py install
+echo -n " 
+Do you want to create a more advanced wordlist? 1(Y) or 2(NO) :"
+read t
 
-mentalist
+if [ $t -eq 1 ]
+
+then
+
+	cd x
+
+	python3 m.py install
+
+	mentalist	
+	
+	
+fi
+
+if [ $t -eq 2 ]
+
+then
+	mkdir /root/Desktop/Awordlist
+	mv *.txt /root/Desktop/Awordlist
+	exit
+	
+	
+fi
+
+# gnome-terminal -- "./r.sh"
+
+
+
+
+
+
 
